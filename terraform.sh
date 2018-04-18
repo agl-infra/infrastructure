c51a8a4d843321d3a25f658b300539d5834f1cff
agl-infra <A136022@agl.com.au>
2018-04-18 13:55:44 +0000

    updated terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -32,8 +32,8 @@ echo -e "\n"
                   -var client_id=${client_id} \
                   -var client_secret=${client_secret} \
                   -var subscription_id=${subscription_id} \
-                  -var tenant_id=${tenant_id}\
-		  -var username=$(username)\
+                  -var tenant_id=${tenant_id} \
+		  -var username=$(username) \
 		  -var password=$(password)
 
 
