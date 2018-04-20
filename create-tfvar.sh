e92dea2d428c03c2e40d3c7a711e946f060b4bd1
agl-infra <38128374+agl-infra@users.noreply.github.com>
2018-04-20 20:40:26 +1000

    reverted back

--- create-tfvar.sh
+++ create-tfvar.sh
@@ -1,19 +1,19 @@
-#${params.userFlag}
-echo -e  'data_resource_group="'${params.ResourceGroupName}'"\n'\
-		'data_vnet="'${params.VnetName}'"\n'\
-        'data_subnet="'${params.SubnetName}'"\n'\
-        'image_publisher="'${params.ImagePublisher}'"\n'\
-        'image_offer="'${params.ImageOffer}'"\n'\
-        'image_sku="'${params.ImageSku}'"\n'\
-        'vm_size="'${params.VmSize}'"\n'\
-        'vm_name="'${params.VmName}'"\n'\
-        'managed_disk_size="'${params.ManagedDiskSize}'"\n'\
-        'subscription_id="'${params.SubscriptionId}'"\n'\
-        'business_owner_tag="'${params.BusinessOwner}'"\n'\
-        'technical_owner_tag="'${params.TechnicalOwner}'"\n'\
-        'cost_code_tag="'${params.CostCode}'"\n'\
-        'schedule_type_tag="'${params.ScheduleType}'"\n'\
-        'project_tag="'${params.Project}'"\n'\
-        'os_name="'${params.OperatingSystem}'"\n'> variables.tfvars
+echo -e  'data_resource_group="'$ResourceGroupName'"\n'\
+		'data_vnet="'$VnetName'"\n'\
+        'data_subnet="'$SubnetName'"\n'\
+        'image_publisher="'$ImagePublisher'"\n'\
+        'image_offer="'$ImageOffer'"\n'\
+        'image_sku="'$ImageSku'"\n'\
+        'vm_size="'$VmSize'"\n'\
+        'vm_name="'$VmName'"\n'\
+        'managed_disk_size="'$ManagedDiskSize'"\n'\
+        'subscription_id="'$SubscriptionId'"\n'\
+        'business_owner_tag="'$BusinessOwner'"\n'\
+        'technical_owner_tag="'$TechnicalOwner'"\n'\
+        'cost_code_tag="'$CostCode'"\n'\
+        'schedule_type_tag="'$ScheduleType'"\n'\
+        'project_tag="'$Project'"\n'\
+        'os_name="'$OperatingSystem'"\n'> variables.tfvars
 
-echo ${params.CrNumber} > cr.txt
+ 
+echo $CrNumber > cr.txt
