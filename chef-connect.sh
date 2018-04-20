899b456623db2d595d0c4dafb1c14ff560aa9ab7
agl-infra <38128374+agl-infra@users.noreply.github.com>
2018-04-20 18:19:41 +1000

    Update chef-connect.sh

--- chef-connect.sh
+++ chef-connect.sh
@@ -1,6 +1,7 @@
-ip=$(head -25 /var/jenkins_home/workspace/InfrastructurePipeline/vinnet/vm-output.json | tail -1 | awk '{print $2}' | sed -e 's/^"//' -e 's/"$//')
+#ip=$(head -25 /var/jenkins_home/workspace/InfrastructurePipeline/vinnet/vm-output.json | tail -1 | awk '{print $2}' | sed -e 's/^"//' -e 's/"$//')
+ip=10.228.10.8
 ssh -t 'root@10.228.10.7' 'sshpass -f password.txt ssh-copy-id agl-user@'$ip''
 ssh -t 'root@10.228.10.7' ip="$ip" bash -s<<'SSH_EOF'
 ssh -t  agl-user@$ip 'echo AGL@12345678 | sudo -S sh -c "echo '10.228.10.5 azsal0047.4uepctfschkudburuyfkqje5le.px.internal.cloudapp.net'>> /etc/hosts"'
 SSH_EOF
-ssh -t 'root@10.228.10.4' 'cd chef-repo && knife bootstrap '$ip' -x agl-user -P AGL@12345678 '
+ssh -t 'root@10.228.10.7' 'cd chef-repo && knife bootstrap '$ip' -N '$ip' -x agl-user -P AGL@12345678 --sudo'
