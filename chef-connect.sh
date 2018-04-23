5ea2f473e23604e13bd50e4ece6a0de128a2e6bb
agl-infra <38128374+agl-infra@users.noreply.github.com>
2018-04-23 19:45:54 +1000

    Update chef-connect.sh

--- chef-connect.sh
+++ chef-connect.sh
@@ -3,4 +3,4 @@ ssh -t 'root@10.228.10.7' 'sshpass -f password.txt ssh-copy-id agl-user@'$ip''
 ssh -t 'root@10.228.10.7' ip="$ip" bash -s<<'SSH_EOF'
 ssh -t  agl-user@$ip 'echo AGL@12345678 | sudo -S sh -c "echo '10.228.10.5 azsal0047.4uepctfschkudburuyfkqje5le.px.internal.cloudapp.net'>> /etc/hosts"'
 SSH_EOF
-ssh -t 'root@10.228.10.7' 'cd chef-repo && knife bootstrap '$ip' -N '$ip' -x agl-user -P AGL@12345678 --sudo'
+ssh -t 'root@10.228.10.7' 'cd chef-repo && knife bootstrap '$ip' -N '$ip' -x agl-user -P AGL@12345678 --sudo --run-list role[agents]'
