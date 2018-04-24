1f2fe58ea004ad5b5337dc2fb94ec851dd13e2ae
agl-infra <38128374+agl-infra@users.noreply.github.com>
2018-04-24 22:59:39 +1000

    Update chef-connect.sh

--- chef-connect.sh
+++ chef-connect.sh
@@ -1,10 +1,10 @@
 ip=$(cat vm-output.txt | grep Private_IP | cut -d '=' -f2 | awk '{$1=$1};1')
-sshpass -p AGL@12345678 ssh-copy-id agl-user@$ip -o StrictHostKeyChecking=no -f
-ssh -t  agl-user@$ip 'echo AGL@12345678 | sudo -S sh -c "echo '10.228.10.5 azsal0047.4uepctfschkudburuyfkqje5le.px.internal.cloudapp.net'>> /etc/hosts"'
+sshpass -p $password ssh-copy-id $username@$ip -o StrictHostKeyChecking=no -f
+ssh -t  $username@$ip 'echo '$password' | sudo -S sh -c "echo '10.228.10.5 azsal0047.4uepctfschkudburuyfkqje5le.px.internal.cloudapp.net'>> /etc/hosts"'
 ##ssh -t 'root@10.228.10.7' 'ssh-keygen -R '$ip''
 #ssh -t 'root@10.228.10.7' 'sshpass -f password.txt ssh-copy-id agl-user@'$ip''
 ## ssh -t 'root@10.228.10.7' 'sshpass -p AGL@12345678 ssh-copy-id agl-user@'$ip''
 # ssh -t 'root@10.228.10.7' ip="$ip" bash -s<<'SSH_EOF'
 # ssh -t  agl-user@$ip 'echo AGL@12345678 | sudo -S sh -c "echo '10.228.10.5 azsal0047.4uepctfschkudburuyfkqje5le.px.internal.cloudapp.net'>> /etc/hosts"'
 # SSH_EOF
-ssh -t 'root@10.228.10.7' 'cd chef-repo && knife bootstrap '$ip' -N '$ip' -x agl-user -P AGL@12345678 --sudo --run-list recipe[installagents]'
+ssh -t 'root@10.228.10.7' 'cd chef-repo && knife bootstrap '$ip' -N '$ip' -x '$username' -P '$password' --sudo --run-list recipe[installagents]'
