af5392c9b64a587da2978265e8425dfdc700375c
agl-infra <38128374+agl-infra@users.noreply.github.com>
2018-04-25 00:15:57 +1000

    Create chef-connect.sh

---
+++ chef-connect.sh
@@ -0,0 +1,4 @@
+ip=$(cat vm-output.txt | grep Private_IP | cut -d '=' -f2 | awk '{$1=$1};1')
+sshpass -p $password ssh-copy-id $username@$ip -o StrictHostKeyChecking=no -f
+ssh -t  $username@$ip 'echo '$password' | sudo -S sh -c "echo '10.228.10.5 azsal0047.4uepctfschkudburuyfkqje5le.px.internal.cloudapp.net'>> /etc/hosts"'
+ssh -t 'root@10.228.10.7' 'cd chef-repo && knife bootstrap '$ip' -N '$ip' -x '$username' -P '$password' --sudo --run-list recipe[installagents]'
